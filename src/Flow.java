96ae71777b49b736fe327304e70225c6a3b1d84e
Gervig <gervig91@gmail.com>
2024-03-13 15:30:21 +0100

    task 1 done

--- src/Flow.java
+++ src/Flow.java
@@ -1,14 +1,25 @@
+//  1.1
 class Flow {
+    //  1.2 + 1.4
     public void methodA() {
-        System.out.println('A');
+        System.out.print('J');
+        System.out.print('a');
+        System.out.print('v');
+        System.out.print('a');
     }
     public void methodB() {
-        System.out.println('B');
+        System.out.print(" ");
     }
     public void methodC() {
-        System.out.println('C');
+        System.out.print('e');
+        System.out.print('r');
+        System.out.print(" ");
     }
     public void methodD() {
-        System.out.println('D');
+        System.out.print('s');
+        System.out.print('j');
+        System.out.print('o');
+        System.out.print('v');
+        System.out.print('t');
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    //  1.3
     public static void main(String[] args) {
         Flow flow1 = new Flow();
         myMainMethod(flow1);
